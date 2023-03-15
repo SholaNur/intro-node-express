4287b25a8b6da1f252dc809d284ec986a9f4dbc4
Shola Nur <shola.nur@gmail.com>
2023-03-14 19:24:46 -0700

    added route

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("hello");
 });
+app.get("/contact", function (req, res) {
+  res.send("Contact Me at - shola.gmail.com");
+});
+app.get("/about", function (req, res) {
+  res.send("Hey there, welcome to my website!");
+});
+
+app.get("/myhobbies", function (req, res) {
+  res.send("<ul><li>Dancing</li> <li>Travelling</li> </ul>  ");
+});
 
 app.listen(3000, function () {
   console.log("server is listening");
